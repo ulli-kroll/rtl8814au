1e709c31264a83907931abd258ed882f4e311f39
Hans Ulli Kroll <ulli.kroll@googlemail.com>
2020-07-15 14:29:01 +0200

    remove unused rtw_BT_efuse_map_write()
    Signed-off-by: Hans Ulli Kroll <ulli.kroll@googlemail.com>

--- include/rtw_efuse.h
+++ include/rtw_efuse.h
@@ -175,7 +175,6 @@ u8	rtw_efuse_access(PADAPTER padapter, u8 bRead, u16 start_addr, u16 cnts, u8 *d
 u8	rtw_efuse_mask_map_read(PADAPTER padapter, u16 addr, u16 cnts, u8 *data);
 u8	rtw_efuse_map_read(PADAPTER padapter, u16 addr, u16 cnts, u8 *data);
 u8	rtw_BT_efuse_map_read(PADAPTER padapter, u16 addr, u16 cnts, u8 *data);
-u8 	rtw_BT_efuse_map_write(PADAPTER padapter, u16 addr, u16 cnts, u8 *data);
 
 u16	Efuse_GetCurrentSize(PADAPTER pAdapter, u8 efuseType, BOOLEAN bPseudoTest);
 u8	Efuse_CalculateWordCnts(u8 word_en);
